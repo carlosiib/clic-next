78edec7ed395f897256e6749285043dc92582199
carlosiib <ntic.carlos@gmail.com>
2024-04-17 20:50:52 -0700

    refactor getAllCustomers function to async await

--- src/app/api/shopify/seed/route.ts
+++ src/app/api/shopify/seed/route.ts
@@ -16,62 +16,64 @@ import {
 // api/shopify/seed
 const CUSTOMERS_URL = process.env.CUSTOMERS_URL!;
 
-let arr: Array<UserProps> = [];
+let arr: ShopifyUser[] = [];
 let paginationURLS: Array<string> = [];
 
 /* Why use axios istead of native fetch?
  * R: Fetch with credentials attatched to the URL will throw an error.
  * https://github.com/elastic/kibana/issues/34847#issuecomment-486126329
  */
-function fetchNextJson(url: string): any {
+async function getAllCustomers(
+  url: string
+): Promise<void | ShopifyUser[]> {
   let next = "";
   let linkHeader: LinkHeader = {};
+  try {
+    const res = await axios.get(url);
 
-  return axios
-    .get(url)
-    .then((response) => {
-      linkHeader = parseLinks(
-        //@ts-ignore
-        response.headers.get("link")
-      );
+    linkHeader = parseLinks(
+      //@ts-ignore
+      res.headers.get("link")
+    );
+    console.log("linkHeader", linkHeader);
 
-      if (!linkHeader?.next) {
-        const finalSearch = paginationURLS.pop();
-        const finalURl = `${CUSTOMERS_URL}&page_info=${finalSearch}`;
-        return axios.get(finalURl).then((pars) => {
-          arr.push(...pars.data.customers);
-        });
-      }
+    if (!linkHeader?.next) {
+      console.log("last");
+      const finalSearch = paginationURLS.pop();
+      const finalURl = `${CUSTOMERS_URL}&page_info=${finalSearch}`;
+      return axios.get(finalURl).then((pars) => {
+        arr.push(...pars.data.customers);
+      });
+    }
 
-      const current_url = new URL(linkHeader.next);
-      const search_params = current_url.searchParams;
-      const id = search_params.get("page_info");
-      if (!id) {
-        console.log("Can't found page_info search param");
-        return;
-      }
-      paginationURLS.push(id);
-      next = `${CUSTOMERS_URL}&page_info=${id}`;
-      return response.data;
-    })
-    .then((data: ShopifyUser) => {
-      if (next) {
-        arr.push(...data.customers);
-        return fetchNextJson(next);
-      } else {
-        return arr;
-      }
-    })
-    .catch(function (err) {
-      console.log("error: " + err);
-    });
+    const current_url = new URL(linkHeader.next);
+    const search_params = current_url.searchParams;
+    const id = search_params.get("page_info");
+    if (!id) {
+      console.log("Can't found page_info search param");
+      return;
+    }
+    paginationURLS.push(id);
+    next = `${CUSTOMERS_URL}&page_info=${id}`;
+
+    const d2 = await res.data;
+    arr.push(...d2.customers);
+    await getAllCustomers(next);
+    return arr;
+  } catch (error) {
+    console.log(
+      "error loading all shopify users: " + error
+    );
+  }
 }
 
 export async function POST() {
   try {
-    const customers = await fetchNextJson(CUSTOMERS_URL);
+    const customers = await getAllCustomers(CUSTOMERS_URL);
+    // console.log("******");
+    // console.log("customers", customers);
 
-    if (customers.length === 0) {
+    if (customers?.length === 0) {
       return NextResponse.json(
         {
           success: false,
@@ -85,6 +87,7 @@ export async function POST() {
     return NextResponse.json(
       {
         success: true,
+        length: customers?.length,
         data: customers,
       },
       { status: 200 }
