d95431b29b7d4beb1a86d2663d51f97c868bd19e
carlosiib <ntic.carlos@gmail.com>
2024-04-23 22:20:04 -0700

    add customers table component

---
+++ src/app/components/Table/page.tsx
@@ -0,0 +1,67 @@
+import { Badge } from "u@/components/ui/badgendefined";
+import { Table as TableContainer, TableBody, TableCell, TableHead, TableHeader, TableRow } from "u@/components/ui/tablendefined";
+
+interface TableProps {
+  customers: {
+    shopifyId: number,
+    firstName: string | null;
+    lastName: string | null,
+    IsPWActivated: boolean,
+    email: string
+  }[],
+  title: string | undefined,
+  searchFor: string | undefined,
+}
+
+export default async function Table({ customers, title, searchFor }: TableProps) {
+
+  return (
+    <>
+      {customers.length === 0 && searchFor &&
+        <>
+          <h4 className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-xl pt-4 text-center">No data available for customer</h4>
+          <p className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-lg text-center text-gray-700">{searchFor}</p>
+        </>
+      }
+      {customers.length !== 0 && (
+        <>
+          {title && <h2 className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-xl py-4">{title}</h2>}
+          <TableContainer>
+            <TableHeader>
+              <TableRow>
+                <TableHead className=" px-0">Name</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead className="text-right">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {customers.map(customer => <>
+                <TableRow key={customer.shopifyId}>
+                  <TableCell className="font-medium px-0">{(customer?.firstName ?? '') + (customer?.lastName ?? '')}</TableCell>
+                  <TableCell>{customer.email}</TableCell>
+                  <TableCell>
+                    {customer.IsPWActivated ?
+                      <Badge className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm shadow-none dark:bg-green-900 dark:text-green-300 hover:bg-green-100 hover:text-green-800 ">
+                        Enabled
+                      </Badge>
+                      :
+                      <Badge className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm shadow-none dark:bg-gray-700 dark:text-gray-300 hover:bg-gray-100 hover:text-gray-800 ">
+                        Pending
+                      </Badge>
+                    }
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <button>...</button>
+                  </TableCell>
+                </TableRow>
+              </>)}
+            </TableBody>
+          </TableContainer >
+        </>
+      )
+      }
+    </>
+
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
 import React from "react";
 import Search from "./components/Search/page";
+import Table from "./components/Table/page";
+import db from "./db/drizzle";
+import { shopify_accounts } from "./db/schema";
+import { desc, eq } from "drizzle-orm";
+import Link from "next/link";
 
-export default function Home() {
+
+// TODO: handle reset search input value?
+export default async function Home({ searchParams }: { [key: string]: string | string[] | undefined }) {
+
+  function getParam(str: string) {
+    if (!searchParams) {
+      return ""
+    }
+    return searchParams[str as keyof typeof searchParams];
+  };
+
+  const searchEmail = getParam('email') as string
+  // console.log({ searchEmail })
+
+  async function getCustomers(query: string | undefined) {
+    console.log(query)
+    try {
+      if (query && query.length) {
+        const data = await db
+          .select()
+          .from(shopify_accounts)
+          .where(eq(shopify_accounts.email, query ?? ''));
+        return data
+      }
+
+      const data = await db
+        .select()
+        .from(shopify_accounts)
+        .orderBy(desc(shopify_accounts.createdAt))
+        .limit(10);
+      return data
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log("Error:", error.message)
+        return []
+      }
+      console.log("Error generic:", error)
+      return []
+    }
+  }
+
+  const customers = await getCustomers(searchEmail)
+
+
+  // TODO: INSTEAD of where = customer, includes? so users dont need to add full email
   return (
     <>
       <Search />
       <main>
-        table
+        {searchEmail && <Link href="/">Clean</Link>}
+        <Table customers={customers} title='Customers' searchFor={searchEmail} />
       </main>
     </>
   );
